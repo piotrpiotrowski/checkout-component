9b263351797b3b4e737b4fdec81ada0143e1cd84
Piotr Piotrowski <piotrpiotrowski87@gmail.com>
2017-09-20 16:46:40 +0200

    Initial commit after generation of project

---
+++ src/main/java/org/siemasoft/checkoutcomponent/CheckoutComponentApplication.java
@@ -0,0 +1,12 @@
+package org.siemasoft.checkoutcomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutComponentApplication.class, args);
+	}
+}
